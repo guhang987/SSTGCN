021a8afbd81370374180c591eb7bc5fbaf73f5f1
guhang987 <610203816@qq.com>
2022-05-26 21:04:30 +0000

    22

---
+++ README.md
@@ -0,0 +1,28 @@
+Deep Learning on Graphs with Keras
+====
+
+Keras-based implementation of SSTGCN.
+
+
+
+Installation
+------------
+
+```python setup.py install```
+
+Dependencies
+-----
+  * python 3.6.13
+  * numpy 1.19.2
+  * keras 2.2.4
+  * TensorFlow 1.12.0
+
+Usage
+-----
+
+```python train.py```
+
+Dataset reference ()
+----------
+
+
---
+++ kegra/utils.py
@@ -0,0 +1,195 @@
+from __future__ import print_function
+import os
+import scipy.sparse as sp
+import numpy as np
+from scipy.sparse.linalg.eigen.arpack import eigsh, ArpackNoConvergence
+# 注意：conda环境为FL
+def data_in_one(inputdata):
+
+    min = np.nanmin(inputdata)
+
+    max = np.nanmax(inputdata)
+
+    outputdata = (inputdata-min)/(max-min)
+
+    return outputdata
+def encode_onehot(labels):
+    classes = set(labels)
+    classes_dict = {c: np.identity(len(classes))[i, :] for i, c in enumerate(classes)}
+    labels_onehot = np.array(list(map(classes_dict.get, labels)), dtype=np.int32)
+    return labels_onehot
+
+
+def get_adj(edges,labels):
+    adj = sp.coo_matrix((np.ones(edges.shape[0]), (edges[:, 0], edges[:, 1])),
+                        shape=(labels.shape[0], labels.shape[0]), dtype=np.float32)
+    adj = adj + adj.T.multiply(adj.T > adj) - adj.multiply(adj.T > adj)
+    return adj
+def load_data(path="data/cora/", dataset="cora"):
+    """Load citation network dataset (cora only for now)"""
+    print('Loading {} dataset...'.format(dataset))
+#     print(os.getcwd())
+    # idx_features_labels = np.genfromtxt("{}{}.content".format(path, dataset), dtype=np.dtype(str))
+    # features = sp.csr_matrix(idx_features_labels[:, 1:-1], dtype=np.float32)
+    features = np.load("{}features.npy".format(path))
+    # features = data_in_one(features)
+    features = sp.csr_matrix(features, dtype=np.float32)
+    # labels = encode_onehot(idx_features_labels[:, -1])
+    # labels =  np.load("{}labels.npy".format(path))
+
+    labels = data_in_one(labels)
+    edges =  np.load("{}edges.npy".format(path))
+    edges2 = np.load("{}edges2.npy".format(path))
+    edges3 = np.load("{}edges3.npy".format(path))
+    # edges = edges2                 
+    adj = get_adj(edges=edges,labels=labels)
+    adj2 = get_adj(edges=edges2,labels=labels)
+    adj3 = get_adj(edges=edges3,labels=labels)
+
+    print('Dataset has {} nodes, {} edges, {} features.'.format(adj.shape[0], edges.shape[0], features.shape[1]))
+
+    return features.todense(), adj,adj2,adj3, labels
+
+
+def load_data_bantian(path="data/cora/", dataset="cora"):
+    """Load citation network dataset (cora only for now)"""
+    print('Loading {} dataset...'.format(dataset))
+    features = np.load("{}features.npy".format(path))[-6:]
+    features = sp.csr_matrix(features, dtype=np.float32)
+    edges = np.array([[0,1],[1,2],[1,3],[2,4],[3,4],[4,5]],dtype='int32')
+    edges2 = np.array([[0,2],[0,3],[1,4],[2,5],[3,5]],dtype='int32')
+    edges3 = np.array([[0,4],[1,5]],dtype='int32')
+    labels = np.zeros((6,1), dtype = 'int32')       
+    adj = get_adj(edges=edges,labels=labels)
+    adj2 = get_adj(edges=edges2,labels=labels)
+    adj3 = get_adj(edges=edges3,labels=labels)
+    print('Dataset has {} nodes, {} edges, {} features.'.format(adj.shape[0], edges.shape[0], features.shape[1]))
+    return features.todense(), adj,adj2,adj3,labels
+
+def load_data_(path="data/cora/", dataset="cora"):
+    """Load citation network dataset (cora only for now)"""
+    print('Loading {} dataset...'.format(dataset))
+
+    idx_features_labels = np.genfromtxt("{}{}.content".format(path, dataset), dtype=np.dtype(str))
+    features = sp.csr_matrix(idx_features_labels[:, 1:-1], dtype=np.float32)
+    labels = encode_onehot(idx_features_labels[:, -1])
+
+    # build graph
+    idx = np.array(idx_features_labels[:, 0], dtype=np.int32)
+    idx_map = {j: i for i, j in enumerate(idx)}
+    edges_unordered = np.genfromtxt("{}{}.cites".format(path, dataset), dtype=np.int32)
+    edges = np.array(list(map(idx_map.get, edges_unordered.flatten())),
+                     dtype=np.int32).reshape(edges_unordered.shape)
+    adj = sp.coo_matrix((np.ones(edges.shape[0]), (edges[:, 0], edges[:, 1])),
+                        shape=(labels.shape[0], labels.shape[0]), dtype=np.float32)
+
+    # build symmetric adjacency matrix
+    adj = adj + adj.T.multiply(adj.T > adj) - adj.multiply(adj.T > adj)
+
+    print('Dataset has {} nodes, {} edges, {} features.'.format(adj.shape[0], edges.shape[0], features.shape[1]))
+
+    return features.todense(), adj, labels
+
+
+def normalize_adj(adj, symmetric=True):
+    if symmetric:
+        d = sp.diags(np.power(np.array(adj.sum(1)), -0.5).flatten(), 0)
+        a_norm = adj.dot(d).transpose().dot(d).tocsr()
+    else:
+        d = sp.diags(np.power(np.array(adj.sum(1)), -1).flatten(), 0)
+        a_norm = d.dot(adj).tocsr()
+    return a_norm
+
+
+def preprocess_adj(adj, symmetric=True):
+    adj = adj + sp.eye(adj.shape[0])
+    adj = normalize_adj(adj, symmetric)
+    return adj
+
+
+def sample_mask(idx, l):
+    mask = np.zeros(l)
+    mask[idx] = 1
+    return np.array(mask, dtype=np.bool)
+
+
+def get_splits(y):
+    idx_train = range(14)
+    idx_val = range(14)
+    idx_test = range(17)
+    y_train = np.zeros(y.shape, dtype=np.int32)
+    y_val = np.zeros(y.shape, dtype=np.int32)
+    y_test = np.zeros(y.shape, dtype=np.int32)
+    y_train[idx_train] = y[idx_train]
+    y_val[idx_val] = y[idx_val]
+    y_test[idx_test] = y[idx_test]
+    train_mask = sample_mask(idx_train, y.shape[0])
+    return y_train, y_val, y_test, idx_train, idx_val, idx_test, train_mask
+
+
+def categorical_crossentropy(preds, labels):
+    return np.mean(-np.log(np.extract(labels, preds)))
+
+
+def accuracy(preds, labels):
+    return np.mean(np.equal(np.argmax(labels, 1), np.argmax(preds, 1)))
+
+
+def evaluate_preds(preds, labels, indices):
+
+    split_loss = list()
+    split_acc = list()
+
+    for y_split, idx_split in zip(labels, indices):
+        split_loss.append(categorical_crossentropy(preds[idx_split], y_split[idx_split]))
+        split_acc.append(accuracy(preds[idx_split], y_split[idx_split]))
+
+    return split_loss, split_acc
+
+
+def normalized_laplacian(adj, symmetric=True):
+    adj_normalized = normalize_adj(adj, symmetric)
+    laplacian = sp.eye(adj.shape[0]) - adj_normalized
+    return laplacian
+
+
+def rescale_laplacian(laplacian):
+    try:
+        print('Calculating largest eigenvalue of normalized graph Laplacian...')
+        largest_eigval = eigsh(laplacian, 1, which='LM', return_eigenvectors=False)[0]
+    except ArpackNoConvergence:
+        print('Eigenvalue calculation did not converge! Using largest_eigval=2 instead.')
+        largest_eigval = 2
+
+    scaled_laplacian = (2. / largest_eigval) * laplacian - sp.eye(laplacian.shape[0])
+    return scaled_laplacian
+
+
+def chebyshev_polynomial(X, k):
+    """Calculate Chebyshev polynomials up to order k. Return a list of sparse matrices."""
+    print("Calculating Chebyshev polynomials up to order {}...".format(k))
+
+    T_k = list()
+    T_k.append(sp.eye(X.shape[0]).tocsr())
+    T_k.append(X)
+
+    def chebyshev_recurrence(T_k_minus_one, T_k_minus_two, X):
+        X_ = sp.csr_matrix(X, copy=True)
+        return 2 * X_.dot(T_k_minus_one) - T_k_minus_two
+
+    for i in range(2, k+1):
+        T_k.append(chebyshev_recurrence(T_k[-1], T_k[-2], X))
+
+    return T_k
+
+
+def sparse_to_tuple(sparse_mx):
+    if not sp.isspmatrix_coo(sparse_mx):
+        sparse_mx = sparse_mx.tocoo()
+    coords = np.vstack((sparse_mx.row, sparse_mx.col)).transpose()
+    values = sparse_mx.data
+    shape = sparse_mx.shape
+    return coords, values, shape
+
+
+
